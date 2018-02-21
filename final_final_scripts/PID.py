bde9fcea0ecf22488afd2bf6575e415735c0b317
L3app <32612883+L3app@users.noreply.github.com>
2018-02-21 10:17:06 +0000

    Add files via upload
    Alan's changes made to PID.py

--- final_final_scripts/PID.py
+++ final_final_scripts/PID.py
@@ -38,7 +38,7 @@ class velControl:
         self._AngVelX = float(coordinates1[0])
         self._AngVelY = float(coordinates1[1])
         self._AngVelZ = float(coordinates1[2])
-        rospy.logwarn("Target velocity is \nx: {} \ny: {} \nz: {}".format(self._targetVelX,self._targetVelY, self._targetVelZ))
+        #rospy.logwarn("Target velocity is \nx: {} \ny: {} \nz: {}".format(self._targetVelX,self._targetVelY, self._targetVelZ))
 
      
     def publishTargetPose(self, stateManagerInstance):
@@ -103,7 +103,7 @@ class stateManager:
  
 def distanceCheck(msg):
     global range1 
-    print("d")
+    #print("d")
     range1 = msg.range 
         
 
@@ -190,7 +190,7 @@ def PID(y, yd, Ki, Kd, Kp, ui_prev, e_prev, limit):
      e_prev = e
      ui_prev = ui
      u = Kp * (e + ui + ud)
-     print("U: ", u)
+     #print("U: ", u)
      if u > limit:
          u = limit
      if u < -limit:
@@ -214,6 +214,17 @@ def main():
     global xpos, ypos, zpos
     xpos, ypos, zpos = 0, 0, 0
     
+    yDesiredDistance = 0.0
+    xDesiredDistance = 0.0
+    zHeight = 0.0
+    flightPhase = 0
+    finalDistance = 6.0
+    yGain = 3 #Alan: the y direction (drift) proportional gain
+    xGain = 3
+    yDesiredDistance = 0.0 #Alan: The desired y coordinate
+    global xDistance
+    global yDistance
+    xDistance, yDistance = 0.0, 0.0
 
     rospy.init_node('navigator')   
     rate = rospy.Rate(20) 
@@ -268,56 +279,135 @@ def main():
 
         
         
-        #while timer1 - time1 < 5:
-            #print(timer1 - time1)
-            #controller.setVel([0,0,0.5])
+        ##while timer1 - time1 < 5:
+        #    #print(timer1 - time1)
+        #    #controller.setVel([0,0,0.5])
         
-        controller.publishTargetPose(stateManagerInstance)
-        stateManagerInstance.incrementLoop()
-        rate.sleep()   
+        #controller.publishTargetPose(stateManagerInstance)
+        #stateManagerInstance.incrementLoop()
+        #rate.sleep()   
 
 
 
-        #print debugging values
-        print("loop: " ,stateManagerInstance.getLoopCount(), " distance: ", range1, " u input: ", u, " zvel: ", velz, " angx: ", x1, " angvelx: ", u1, " angy: ", y1, " angvely: ", u2, " angx: ", z1, " angvelx: ", u3) 
+        ##print debugging values
+        #print("loop: " ,stateManagerInstance.getLoopCount(), " distance: ", range1, " u input: ", u, " zvel: ", velz, " angx: ", x1, " angvelx: ", u1, " angy: ", y1, " angvely: ", u2, " angx: ", z1, " angvelx: ", u3) 
 
-        if stateManagerInstance.getLoopCount() > 100:
-           #hover pid
-            zprev = z
-            z = range1
-            xprev = x
-            x = xpos            
-            deltaz = z - zprev
-            deltax = x - xprev
-            if deltax == 0:
-                deltax = 1
-                deltaz = numpy.inf
-            if range1 < 1.4 or range1 > 1.8:
-                switch = 0
-            elif abs(range1 - 1.5) < tol+0.05 and switch == 0:
-                switch = 1
+        #if stateManagerInstance.getLoopCount() > 100:
+            ##hover pid
+            #zprev = z
+            #z = range1
+            #xprev = x
+            #x = xpos            
+            #deltaz = z - zprev
+            #deltax = x - xprev
+            #if deltax == 0:
+                #deltax = 1
+                #deltaz = numpy.inf
+            #if range1 < 1.4 or range1 > 1.8:
+                #switch = 0
+            #elif abs(range1 - 1.5) < tol+0.05 and switch == 0:
+                #switch = 1
+                #timer2 = timer1
+            #elif range1 > 1.3 and range1 < 1.8:
+                #switch = 2
+            #if switch == 0:
+                #controller.setVel([0,yGain*(yDesiredDistance - ypos),u],[0,0,0])
+                #u, ui_prev, e_prev = PID(range1, 1.5, 1, 1, 1, ui_prev, e_prev, 0.5) 
+            #elif switch == 1 and abs(timer2-timer1) < 2:
+                #controller.setVel([0,yGain*(yDesiredDistance - ypos),0],[0,0,0])
+            #elif switch == 2 and abs(timer2-timer1) > 2:
+                #controller.setVel([xcontrol,yGain*(yDesiredDistance - ypos),u],[0,0,0])
+                #u, ui_prev, e_prev = PID(range1, 1.5, 1, 1, 1, ui_prev, e_prev, 0.5)
+                ##xcontrol = 0.5 - (0.3/90)*math.degrees(numpy.arctan(abs(deltaz/deltax)))
+                #xcontrol = 0.5 - (0.3/0.1)*numpy.clip(abs(range1 - 1.5),0,0.1)
+
+
+            #"""#stable y pid
+            #controller.setAngVel([0,u2,0])
+            #u2, ui_prev2, e_prev2 = PID(y1, 0, 1, 1, 1, ui_prev2, e_prev2)"""
+        #rest on ground phase
+        if flightPhase == 0:
+            controller.setVel([0,0,0],[0,0,0])
+            controller.publishTargetPose(stateManagerInstance)
+            stateManagerInstance.incrementLoop()
+            rate.sleep()    #sleep at the set rate
+        #takeoff phase
+        if flightPhase == 1:
+            zHeight = zHeight + 0.02
+            controller.setVel([xGain*(xDesiredDistance - xpos),yGain*(yDesiredDistance - ypos),zHeight-range1],[0,0,0])
+            controller.publishTargetPose(stateManagerInstance)
+            stateManagerInstance.incrementLoop()
+            rate.sleep()    #sleep at the set rate
+        #hover phase
+        if flightPhase == 2:
+            controller.setVel([xGain*(xDesiredDistance - xpos),yGain*(yDesiredDistance - ypos),zHeight-range1],[0,0,0])
+            controller.publishTargetPose(stateManagerInstance)
+            stateManagerInstance.incrementLoop()
+            rate.sleep()    #sleep at the set rate
+        #forward flight phase
+        if flightPhase == 3:
+            #xDesiredDistance = xDesiredDistance + 0.02
+            #controller.setVel([3*(xDesiredDistance - xpos),yGain*(yDesiredDistance - ypos),2*(zHeight-range1)])
+            controller.setVel([xcontrol,yGain*(yDesiredDistance - ypos),u],[0,0,0])
+            u, ui_prev, e_prev = PID(range1, 1.5, 1, 1, 1, ui_prev, e_prev, 0.5)
+                
+            xcontrol = 0.3 - (0.2/0.1)*numpy.clip(abs(range1 - 1.5),0,0.1) #this line is unstable when x0 > 0.4, at least on VM workstation (use player!)
+            controller.publishTargetPose(stateManagerInstance)
+            stateManagerInstance.incrementLoop()
+            rate.sleep()    #sleep at the set rate    
+                
+        #landing A phase
+        if flightPhase == 4:
+
+            zHeight = zHeight*0.95 - 0.005
+            controller.setVel([xGain*(xDesiredDistance - xpos),yGain*(yDesiredDistance - ypos),2*(zHeight-range1)],[0,0,0])
+            controller.publishTargetPose(stateManagerInstance)
+            stateManagerInstance.incrementLoop()
+            rate.sleep()    #sleep at the set rate      
+        #landing B phase    
+        if flightPhase == 5:
+            controller.setVel([0,0,-0.2],[0,0,0])
+            controller.publishTargetPose(stateManagerInstance)
+            stateManagerInstance.incrementLoop()
+            rate.sleep()    #sleep at the set rate  
+        
+        
+        #change from state 0 to 1 if count >100
+        if stateManagerInstance.getLoopCount() > 100:   #need to send some position data before we can switch to offboard mode otherwise offboard is rejected
+            if flightPhase == 0:
+                flightPhase = 1
+                print(1)
+                #zHeight = 1.5
+            stateManagerInstance.offboardRequest()  #request control from external computer
+            stateManagerInstance.armRequest()   #arming must take place after offboard is requested
+        #change from phase 1 to 2 if at z=1.5m
+        if zHeight >= 1.5: #stateManagerInstance.getLoopCount() > 100:   #need to send some position data before we can switch to offboard mode otherwise offboard is rejected
+            if flightPhase == 1:
+                zHeight = 1.5
+                flightPhase = 2 
+                print(2)
                 timer2 = timer1
-            elif range1 > 1.3 and range1 < 1.8:
-                switch = 2
-            if switch == 0:
-                controller.setVel([0,0,u],[0,0,0])
-                u, ui_prev, e_prev = PID(range1, 1.5, 1, 1, 1, ui_prev, e_prev, 0.5) 
-            elif switch == 1 and abs(timer2-timer1) < 2:
-                controller.setVel([0,0,0],[0,0,0])
-            elif switch == 2 and abs(timer2-timer1) > 2:
-                controller.setVel([xcontrol,0,u],[0,0,0])
-                u, ui_prev, e_prev = PID(range1, 1.5, 1, 1, 1, ui_prev, e_prev, 0.5)
-                xcontrol = 0.5 - (0.3/90)*math.degrees(numpy.arctan(abs(deltaz/deltax)))
-
-
-            """#stable y pid
-            controller.setAngVel([0,u2,0])
-            u2, ui_prev2, e_prev2 = PID(y1, 0, 1, 1, 1, ui_prev2, e_prev2)"""
- 
+        #change from phase 2 to 3 after two seconds
+        if flightPhase == 2:
+            if (timer1 - timer2) > 2:
+				flightPhase = 3
+				print(3)
+        #print(xpos)
+        #change from phase 3 to 4 after reaching finalDistance (=6m likely)
+        if flightPhase == 3:
+            if xpos >= finalDistance:
+                flightPhase = 4
+                print(4)
+                xDesiredDistance = finalDistance
+        #change from phase 4 to 5 when z = 0.2m
+        if flightPhase == 4:
+            if zHeight <= 0.2:
+                flightPhase = 5
+                print(5)
             
 
-            stateManagerInstance.offboardRequest()  
-            stateManagerInstance.armRequest()  
+            #stateManagerInstance.offboardRequest()  
+            #stateManagerInstance.armRequest()  
     rospy.spin()  
 
     
